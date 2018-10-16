991e09e03b959b43c5755e69a709fcbb41d67e4f
Fabricio Fernandes Freitas <fffreitas2@stefanini.com>
2018-10-16 02:39:24 -0300

    Comite Iniciaç

---
+++ eureka-pessoa/src/main/java/com/stefanini/dto/EnderecoDTO.java
@@ -0,0 +1,24 @@
+package com.stefanini.dto;
+
+import java.io.Serializable;
+
+public class EnderecoDTO  implements Serializable{
+
+
+	private static final long serialVersionUID = 1L;
+	private String cidade;
+	private String bairro;
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	
+}
